ffadd88eddd519e7e83714c7666955d8c0ab1faf
HieuTom <giaphieu2006@gmail.com>
2025-06-26 01:14:06 +0700

    add link to img, update some back-end programs

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 // Taoj banner slider
     const banner_show = document.querySelector(".banner_show");
+    const link_banner = document.querySelector(".move");
     const list = document.querySelector(".list");
     const prev = document.querySelector(".prev");
     const next = document.querySelector(".next");
@@ -21,6 +22,20 @@
             const src = items[index].getAttribute("src");
             banner_show.setAttribute("src", src);
             items[index].classList.add("active");
+            fetch("../../assets/all_movie.json")
+            .then(res => res.json())
+            .then(data => {
+                const movie = data.find(movie => movie.poster === src);
+                if (movie) {
+                    link_banner.setAttribute("href", movie.link);
+                } else {
+                    link_banner.innerHTML = `<p>Thông tin không có sẵn</p>`;
+                }
+            })
+            .catch(error => {
+                console.log("Error fetching movie data:", error);
+                link_banner.innerHTML = `<p>Thông tin không có sẵn</p>`;
+            });
         }
     }
 
@@ -61,6 +76,7 @@ setInterval(autoSlide, 4000);
 // Cuộn ngang danh sách phim đề cử maf khoong caanf giuwx shift
 const scrollContainer_r = document.querySelector('.recommended_movie_list');
 const scrollContainer_h = document.querySelector('.hot_movie_list');
+const scrollContainer_l = document.querySelector('.list');
 scrollContainer_r.addEventListener('wheel', function (evt) { //recommended_movie_list
   evt.preventDefault(); // ngăn cuộn dọc
   scrollContainer_r.scrollLeft += evt.deltaY * 2.5; // cuộn ngang theo chiều cuộn chuột
@@ -68,5 +84,8 @@ scrollContainer_r.addEventListener('wheel', function (evt) { //recommended_movie
 scrollContainer_h.addEventListener('wheel', function (evt) { //hot_movie_list
   evt.preventDefault(); 
   scrollContainer_h.scrollLeft += evt.deltaY * 2.5; 
-
+});
+scrollContainer_l.addEventListener('wheel', function (evt) { //list
+  evt.preventDefault();
+  scrollContainer_l.scrollLeft += evt.deltaY * 2.5;
 });
--- js/import_film.js
+++ js/import_film.js
@@ -1,12 +1,13 @@
 /*banner slider*/
-fetch("banner_movie.json")
+fetch("../assets/all_movie.json")
   .then(res => res.json())
   .then(data => {
     const list = document.querySelector(".list");
-    data.forEach(movie => {
+    data.filter(movie => movie.type === "banner")
+    .forEach(movie => {
       const movie_item = document.createElement("div");
       movie_item.innerHTML = `
-        <img class="item" src="${movie.poster}" alt="${movie.title}" width="200">
+        <a href="${movie.link}"><img class="item" src="${movie.poster}" alt="${movie.title}" width="200"></a>
       `;
       list.appendChild(movie_item);
     });
@@ -17,16 +18,18 @@ fetch("banner_movie.json")
 
 
 // Recommended movie list
-  fetch("recommended_movie.json")
+  fetch("../assets/all_movie.json")
     .then(res => res.json())
     .then(data => {
     const recommended_movie_list = document.querySelector(".recommended_movie_list");
-    data.forEach(recommended_movie =>{
+    data.filter(movie => movie.type === "recommended")
+    .forEach(recommended_movie =>{
       const recommended_movie_item = document.createElement("div");
       recommended_movie_item.innerHTML = `
-        <img class="item" src="${recommended_movie.poster_link}" alt="${recommended_movie.title}" width="200">
+        <a href="${recommended_movie.link}"><img src="${recommended_movie.poster}" alt="${recommended_movie.title}" width="200">
         <p>${recommended_movie.title} (${recommended_movie.year})</p>
-        `;
+        </a>
+      `;
       recommended_movie_list.appendChild(recommended_movie_item);
     })
   })
@@ -35,15 +38,17 @@ fetch("banner_movie.json")
   });
 
   // Hot movie list
-  fetch("hot_movie.json")
+  fetch("../assets/all_movie.json")
   .then(res => res.json())
   .then(data => {
     const hot_movie_list = document.querySelector(".hot_movie_list");
-    data.forEach(hot_movie => {
+    data.filter(movie => movie.type === "hot")
+    .forEach(hot_movie => {
       const hot_movie_item = document.createElement("div");
       hot_movie_item.innerHTML = `
-        <img class="item" src="${hot_movie.poster_link}" alt="${hot_movie.title}" width="200">
+        <a href="${hot_movie.link}"><img src="${hot_movie.poster}" alt="${hot_movie.title}" width="200">
         <p>${hot_movie.title} (${hot_movie.year})</p>
+        </a>
       `;
       hot_movie_list.appendChild(hot_movie_item);
     })
@@ -52,38 +57,20 @@ fetch("banner_movie.json")
     console.log("Error fetching hot movie data:", error);
   });
   // All movie list (gop ca recommended + hot + banner)
-fetch("recommended_movie.json")
+fetch("../assets/all_movie.json")
   .then(res => res.json())
-  .then(recommended_data => {
-    fetch("hot_movie.json")
-      .then(res => res.json())
-      .then(hot_data => {
-        fetch("banner_movie.json")
-          .then(res => res.json())
-          .then(banner_data => {
-            const all_movie_list = document.querySelector(".all_movie_list");
-            //gop tat ca du lieu
-            const all_movies = [...recommended_data, ...hot_data, ...banner_data];
-            // hien thi tung phim
-            all_movies.forEach(movie => {
-              const movie_item = document.createElement("div");
-              const poster = movie.poster_link || movie.poster;
-
-              movie_item.innerHTML = `
-                <img class="item" src="${poster}" alt="${movie.title}" width="200">
-                <p>${movie.title} (${movie.year})</p>
-              `;
-              all_movie_list.appendChild(movie_item);
-            });
-          })
-          .catch(error => {
-            console.log("Error fetching banner movie data:", error);
-          });
-      })
-      .catch(error => {
-        console.log("Error fetching hot movie data:", error);
-      });
+  .then(data => {
+    const all_movie_list = document.querySelector(".all_movie_list");
+    data.forEach(movie => {
+      const movie_item = document.createElement("div");
+      movie_item.innerHTML = `
+        <a href="${movie.link}"><img class="item" src="${movie.poster}" alt="${movie.title}" width="200">
+        <p>${movie.title} (${movie.year})</p>
+        </a>
+      `;
+      all_movie_list.appendChild(movie_item);
+    })
   })
   .catch(error => {
-    console.log("Error fetching recommended movie data:", error);
+    console.log("Error fetching all movie data:", error);
   });
